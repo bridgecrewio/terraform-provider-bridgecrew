3bfce1f2fe0e6ec84adacde1c6741dcb65afc5af
James Woolfenden <james.woolfenden@gmail.com>
2021-09-01 16:58:41 +0100

    data model for policy

--- bridgecrew/resource_policy.go
+++ bridgecrew/resource_policy.go
@@ -2,6 +2,7 @@ package bridgecrew
 
 import (
 	"context"
+	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -14,120 +15,132 @@ func resourcePolicy() *schema.Resource {
 		UpdateContext: resourePolicyUpdate,
 		DeleteContext: resourcePolicyDelete,
 		Schema: map[string]*schema.Schema{
-			"policies": {
+			//"policies": {
+			//	Type:     schema.TypeSet,
+			//	Computed: false,
+			//	Required: true,
+			//	Elem: &schema.Resource{
+			//		Schema: map[string]*schema.Schema{
+			"cloud_provider": {
+				Type:     schema.TypeString,
+				Computed: false,
+				Required: true,
+			},
+			//"id": {
+			//  Type:     schema.TypeString,
+			//  Computed: true,
+			//},
+			"title": {
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Required: true,
+			},
+			"descriptive_title": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"constructive_title": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"severity": {
+				Type:     schema.TypeString,
+				Required: true,
+				//todo
+				// needs to one of the severity classes not a free string field
+			},
+			"category": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"resource_types": {
+				Type:     schema.TypeList,
+				Required: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"accountsdata": {
 				Type:     schema.TypeSet,
-				Computed: true,
+				Optional: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"provider": {
-							Type:     schema.TypeString,
-							Computed: false,
+						"repository": {
 							Required: true,
-						},
-						//"id": {
-						//  Type:     schema.TypeString,
-						//  Computed: true,
-						//},
-						"title": {
 							Type:     schema.TypeString,
+						},
+						"amounts": {
 							Required: true,
-						},
-						"descriptivetitle": {
-							Type:     schema.TypeString,
-							Optional: true,
-						},
-						"constructivetitle": {
-							Type:     schema.TypeString,
-							Optional: true,
-						},
-						"severity": {
-							Type:     schema.TypeString,
-							Required: true,
-						},
-						"category": {
-							Type:     schema.TypeString,
-							Required: true,
-						},
-						"resourcetypes": {
-							Type:     schema.TypeList,
-							Required: true,
+							Type:     schema.TypeMap,
 							Elem: &schema.Schema{
-								Type:    schema.TypeString,
-								Default: "",
+								Type: schema.TypeInt,
 							},
 						},
-						"accountsdata": {
-							Type:     schema.TypeSet,
-							Optional: true,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"repository": {
-										Required: true,
-										Type:     schema.TypeString,
-									},
-									"amounts": {
-										Required: true,
-										Type:     schema.TypeMap,
-										Elem: &schema.Schema{
-											Type: schema.TypeInt,
-										},
-									},
-									"lastupdatedate": {
-										Computed: true,
-										Type:     schema.TypeString,
-									},
-								},
-							},
-						},
-						"guideline": {
-							Type:     schema.TypeString,
+						"lastupdatedate": {
 							Computed: true,
-						},
-						"iscustom": {
-							Type:     schema.TypeBool,
-							Computed: true,
-						},
-						"conditionquery": {
 							Type:     schema.TypeString,
-							Required: true,
-						},
-						"benchmarks": {
-							Type:     schema.TypeSet,
-							Optional: true,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"benchmark": {
-										Type:     schema.TypeString,
-										Required: true,
-									},
-									"version": {
-										Type:     schema.TypeList,
-										Required: true,
-										Elem: &schema.Schema{
-											Type: schema.TypeString,
-										},
-									},
-								},
-							},
-						},
-						"createdby": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"code": {
-							Type:     schema.TypeString,
-							Required: true,
 						},
 					},
 				},
 			},
+			"guideline": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"iscustom": {
+				Type:     schema.TypeBool,
+				Required: true,
+			},
+			"condition_query": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"benchmarks": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"benchmark": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"version": {
+							Type:     schema.TypeList,
+							Required: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+					},
+				},
+			},
+			"createdby": {
+				Type:     schema.TypeString,
+				Computed: true,
+				//todo
+				// should this not be estimated automatically from the TOKEN?
+			},
+			"code": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			//	},
+			//	},
+			//	},
 		},
 	}
 }
 
 func resourcePolicyCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	// Warning or errors can be collected in a slice type
+
+	//client := &http.Client{Timeout: 10 * time.Second}
 	var diags diag.Diagnostics
+
+	policies := d.Get("policies").([]interface{})
+
+	log.Print(policies)
+
+	//d.SetId(strconv.Itoa(o.ID))
 
 	return diags
 }
@@ -149,3 +162,29 @@ func resourcePolicyDelete(ctx context.Context, d *schema.ResourceData, m interfa
 
 	return diags
 }
+
+// CreateOrder - Create new order
+//func (c *Client) CreatePolicy(PolicyItems []PolicyItem) (*Policy, error) {
+//	rb, err := json.Marshal(PolicyItem)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	req, err := http.NewRequest("POST", fmt.Sprintf("%s/orders", c.HostURL), strings.NewReader(string(rb)))
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	body, err := c.doRequest(req)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	policy := Policy{}
+//	err = json.Unmarshal(body, &policy)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	return &policy, nil
+//}
--- main.tf
+++ main.tf
@@ -17,19 +17,40 @@ terraform {
   }
 }
 
-output "repos" {
-  value = data.bridgecrew_repositories.all
-}
+//output "repos" {
+//  value = data.bridgecrew_repositories.all
+//}
 
 
-output "suppression" {
-  value = data.bridgecrew_suppressions.all
-}
+//output "suppression" {
+//  value = data.bridgecrew_suppressions.all
+//}
 
-output "polices" {
-  value = data.bridgecrew_policies.all
-}
+//output "polices" {
+//  value = data.bridgecrew_policies.all
+//}
 
 //output "branches" {
 // value = data.bridgecrew_repository_branches.all
 //}
+
+
+resource "bridgecrew_policy" "new" {
+  count           = 2
+  cloud_provider  = "aws"
+  title           = "my first test ${count.index}"
+  severity        = "CRITICAL"
+  category        = "LOGGING"
+  resource_types  = ["aws_instance", "aws_s3_bucket"]
+  iscustom        = true
+  code            = "echo 'hello world'"
+  condition_query = "some shizzle"
+  benchmarks {
+    benchmark = "free text for now"
+    version   = ["1.1", "1.2"]
+  }
+}
+
+output "policy" {
+  value = bridgecrew_policy.new
+}
